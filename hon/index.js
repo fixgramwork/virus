8cf02f0ec1536372b7d17d79a9917a0cd83d326d
fixgram <154101599+fixgramwork@users.noreply.github.com>
2024-10-30 21:35:57 +0900

    Add files via upload

---
+++ hon/index.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a,i) => {
+	a.src = 'https://s3.orbi.kr/data/file/united2/a9697afd712e4edf8f2acb5b9718aa5d.jpeg'
+    })
+}, 500);
