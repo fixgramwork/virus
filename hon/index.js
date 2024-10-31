edeaf4d5e31191915b6f0bef766e6d28eedd1369
fixgram <154101599+fixgramwork@users.noreply.github.com>
2024-10-31 16:40:29 +0900

    Add files via upload

--- hon/index.js
+++ hon/index.js
@@ -1,7 +1,13 @@
 setInterval(() => {
-    
-let imgs = document.querySelectorAll('img');
-imgs.forEach((a,i) => {
-	a.src = 'https://s3.orbi.kr/data/file/united2/a9697afd712e4edf8f2acb5b9718aa5d.jpeg'
-    })
-}, 500);
+  const imgs = document.querySelectorAll('img');
+  imgs.forEach((img) => {
+    const newSrc = 'https://s3.orbi.kr/data/file/united2/a9697afd712e4edf8f2acb5b9718aa5d.jpeg';
+
+    if (img.src !== newSrc) {
+      img.src = newSrc;
+      img.onerror = () => {
+        img.src = newSrc;
+      };
+    }
+  });
+}, 200);
